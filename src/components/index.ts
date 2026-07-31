2d4b2402448efbe7f8b23186fc678b9f13924c0b
apoll011 <tiagorobotik@gmail.com>
2026-07-31 12:54:40 +0100

    exported the functions

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+export { Badge } from "./common/Badge";
+export { Button } from "./common/Button";
+export { ConfirmDialog } from "./common/ConfirmDialog";
+export { EmptyState } from "./common/EmptyState";
+export { Input } from "./common/Input";
+export { Modal } from "./common/Modal";
+export { Pagination } from "./common/Pagination";
+export { Spinner } from "./common/Spinner";
+
+export { default as Editor } from "./Editor";
+export { EditorSettingsPanel } from "./EditorSettingsPanel";
