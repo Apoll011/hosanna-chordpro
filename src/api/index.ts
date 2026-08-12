14657a9da2100da1088237e7323dc902c4aaabe8
apoll011 <tiagorobotik@gmail.com>
2026-08-12 21:21:15 +0100

    Removed Settings

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 export { foldersApi } from "./folders";
 export { printApi } from "./print";
 export { servicesApi } from "./services";
-export { settingsApi } from "./settings";
 export { parseSong, songsApi } from "./songs";
 export { syncApi } from "./sync";
 
