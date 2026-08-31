75bcbcde135b2e7d50a1b04c3383b0b58868853d
apoll011 <tiagorobotik@gmail.com>
2026-08-31 18:00:52 +0100

    minor fix

--- src/editor/mode-chordpro.ts
+++ src/editor/mode-chordpro.ts
@@ -9,7 +9,11 @@ export async function registerChordproMode(aceInstance?: any): Promise<void> {
     }
   }
 
-  if (!ace || typeof ace.define !== "function" || (ace as any)._chordproModeRegistered) {
+  if (
+    !ace ||
+    typeof ace.define !== "function" ||
+    (ace as any)._chordproModeRegistered
+  ) {
     return;
   }
 
@@ -109,7 +113,7 @@ export async function registerChordproMode(aceInstance?: any): Promise<void> {
               ],
               regex:
                 rStart +
-                "(title|t|subtitle|st|artist|a|composer|lyricist|ccli|translator|youtube|chorus|copyright|album|year|key|k|time|tempo|duration|capo|meta|c|comment|chord|define|song_number|x_[a-zA-Z0-9_]+)" +
+                "(title|t|subtitle|st|artist|a|composer|lyricist|song_number|ccli|translator|youtube|chorus|copyright|album|year|key|k|original_key|time|tempo|duration|capo|meta|c|comment|chord|define|x_[a-zA-Z0-9_]+)" +
                 rSep +
                 "(.*?)" +
                 rEnd,
